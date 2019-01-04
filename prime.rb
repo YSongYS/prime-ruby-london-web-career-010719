d3297a395fa721827950a1a41a8ef38b293be2ef
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 15:56:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  answer = false
+  if num==0 || num==1 || num<0
+    false
+  elsif num==2
+    true
+  else
+    for denominator in 2..(num-1) do
+    answer = answer || (num % denominator == 0)
+  end
+  return !answer
+  end
+end
