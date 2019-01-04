fded3c38fbf9ac62aed94a3ede2e4e330d99d8b4
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 15:44:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 # Add  code here!
 def prime?(num)
-  Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
+  answer = false
+  if num==0 || num==1 || num<0
+    false
+  else for denominator in 1..num do
+    answer = answer || (num % denominator == 0)
+    return anwer
+  end
+  end
 end
