c5d04809c33ae48eea0b9888b2885bd8a8b590fd
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 15:48:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,9 @@ def prime?(num)
   answer = false
   if num==0 || num==1 || num<0
     false
-  else for denominator in 1..(num-1) do
+  elsif num==2
+    true
+  else for denominator in 2..(num-1) do
     answer = answer || (num % denominator == 0)
     return !answer
   end
